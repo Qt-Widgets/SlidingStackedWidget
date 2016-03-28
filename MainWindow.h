34b0f7012ae219ab2d3ffc840b130288f425aaa3
Shujaat Ali Khan <shujaatalikhan@live.com>
2016-03-28 12:56:28 +0400

    Added files via upload

---
+++ MainWindow.h
@@ -0,0 +1,52 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include "SlidingStackedWidget.h"
+#include <QtGui>
+#include <QMainWindow>
+#include <QPushButton>
+#include <QCheckBox>
+#include <QListWidget>
+#include <QSlider>
+#include <QComboBox>
+#include <QLabel>
+#include <QLCDNumber>
+#include <QVBoxLayout>
+#include <QGridLayout>
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+protected:
+    void createGuiControlComponents();
+    void createMainLayout();
+    void createSubSlidingWidgets();
+    void createConnections();
+    void createSlidingStackedWidget();
+    QPushButton *buttonNext;
+    QPushButton *buttonPrev;
+    QCheckBox *checkWrap;
+    QCheckBox *checkVertical;
+    QSlider *slideSpeed;
+    QComboBox *listAll;
+    QLabel *speedLabel;
+    QLCDNumber *speedDisplay;
+
+    SlidingStackedWidget *slidingStacked;
+    QVBoxLayout *mainLayout;
+    QGridLayout *controlPaneLayout;
+    QWidget *centralWidget;
+    QWidget *slideWidget1;
+    QWidget *slideWidget2;
+    QWidget *slideWidget3;
+    QWidget *slideWidget4;
+
+    int animTime;
+};
+
+#endif // MAINWINDOW_H
---
+++ SlidingStackedWidget.pro
@@ -0,0 +1,15 @@
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+# -------------------------------------------------
+# Project created by QtCreator 2010-03-21T15:45:08
+# -------------------------------------------------
+
+TARGET = SlidingStackedWidget
+TEMPLATE = app
+SOURCES += main.cpp \
+    MainWindow.cpp \
+    SlidingStackedWidget.cpp
+HEADERS += MainWindow.h \
+    SlidingStackedWidget.h
